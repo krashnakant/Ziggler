37f6bdce9499ab33da7c8e0499ebeef703f6a184
chaurasiak <krashna.chaurasia@bahag.com>
2026-02-07 22:17:13 +0100

    Initial Commit

---
+++ Ziggler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ziggler
+//
+//  Created by Krashna Chaurasia on 07.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ziggler/ZigglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZigglerApp.swift
+//  Ziggler
+//
+//  Created by Krashna Chaurasia on 07.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ZigglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
